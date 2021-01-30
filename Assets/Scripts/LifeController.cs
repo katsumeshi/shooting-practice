dd51cee263a1b5c1f681b70d8433e699fc325214
Yuki Matsushita <katsumeshi@gmail.com>
2021-01-29 23:32:14 -0800

    first commit

---
+++ Assets/Scripts/LifeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour
+{
+    public GameObject lifePrefab;
+
+	void Start () {
+        RocketController rocket = GameObject.Find("rocket").GetComponent<RocketController>();
+        Vector2 lb = Camera.main.ScreenToWorldPoint(Vector3.zero);
+		Vector2 rt = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        for (int i = 0; i < rocket.life; i++) {
+            Instantiate (lifePrefab, new Vector3 ((0.5f * i) + lb.x + 0.3f, rt.y - 0.3f, 0), Quaternion.identity);
+        }
+	}
+}
---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public int life = 3;
+
+	void Update () {
+		
+		JoystickController jc = GameObject.Find("Fixed Joystick").GetComponent<JoystickController>();
+
+		Vector2 playerPos = transform.position;
+		Vector2 size = GetComponent<SpriteRenderer>().bounds.size;
+		Vector2 screenLeftBottom = Camera.main.ScreenToWorldPoint(Vector3.zero);
+		Vector2 screenRightTop = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+		if (playerPos.x - size.x/2 > screenLeftBottom.x) {
+			if (jc.move == -1 || Input.GetKey (KeyCode.LeftArrow)) {
+				transform.Translate (-0.1f, 0, 0);
+			}
+		}
+		if (playerPos.x + size.x/2 < screenRightTop.x) {
+			if (jc.move == 1 || Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate ( 0.1f, 0, 0);
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate (bulletPrefab, transform.position, Quaternion.identity);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+
+		if (life > 0) {
+			GameObject[] objects = GameObject.FindGameObjectsWithTag("Life");
+			Destroy(objects[life-1]);
+		}
+
+		if (life <= 1) {
+			GameObject.Find ("Canvas").GetComponent<UIController>().GameOver ();
+		}
+		life -= 1;
+	}
+}
